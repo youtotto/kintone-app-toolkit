a256a90234196e02e10c9bbc6be6c53300c5b395
youtotto <spruce.onetwo34@hotmail.co.jp>
2025-10-11 13:47:30 +0900

    miss v1.3.0→1.2.0

--- kintoneAppToolkit.user.js
+++ kintoneAppToolkit.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         kintone App Toolkit: Health Check + Field Inventory + Filter
 // @namespace    https://github.com/youtotto/kintone-app-toolkit
-// @version      1.3.0
+// @version      1.2.0
 // @description  kintoneアプリのヘルスチェック、フィールド一覧、一覧のフィルター/ソート表示
 // @match        https://*.cybozu.com/k/*/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=cybozu.com
